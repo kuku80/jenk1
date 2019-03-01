cbf5b6c027e68d7c4be2888eecfdbbf4e44c38c8
kuku80 <46440543+kuku80@users.noreply.github.com>
2019-03-01 20:01:49 +0200

    Update runExe.groovy

--- vars/runExe.groovy
+++ vars/runExe.groovy
@@ -3,7 +3,7 @@
 def call(String path) {
   echo "Hello, ${path}"
   def sout = new StringBuilder(), serr = new StringBuilder()
-  def args = ['cmd', "${path}"]
+  def args = ['cmd',"/c" "${path}"]
   def proc = new ProcessBuilder( args )
   Process process = proc.start()
   process.consumeProcessOutput( sout, serr )
