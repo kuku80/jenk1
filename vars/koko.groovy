e895722ba82f038ea6e3bb726be075839c9f7397
kuku80 <46440543+kuku80@users.noreply.github.com>
2019-03-02 23:11:54 +0200

    Update koko.groovy

--- vars/koko.groovy
+++ vars/koko.groovy
@@ -3,7 +3,7 @@
 def call(String path) {
   echo "Hello, ${path}"
   def sout = new StringBuilder(), serr = new StringBuilder()
-  def args = ['hello']
+  def args = ['ipconfig']
   def proc = new ProcessBuilder( args )
   //Runtime.runtime.exec("cmd /c ipconfig")
   Process process = proc.start()
