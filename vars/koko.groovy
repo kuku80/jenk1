65ae32434591af607fd1644cccad8bf377717802
kuku80 <46440543+kuku80@users.noreply.github.com>
2019-03-02 23:58:47 +0200

    Update koko.groovy

--- vars/koko.groovy
+++ vars/koko.groovy
@@ -11,6 +11,6 @@ def call(String path) {
   //process.waitForOrKill( 2000 )
  //println System.getenv("PATH")
  // println "${path}".execute().text
-  bat 'sc create'
+  bat "sc create koko binpath=${path}"
 }
 
