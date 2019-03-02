aedddbeca4570b951e19e8903107ed57ae2af247
kuku80 <46440543+kuku80@users.noreply.github.com>
2019-03-02 23:42:01 +0200

    Update koko.groovy

--- vars/koko.groovy
+++ vars/koko.groovy
@@ -2,14 +2,15 @@
 
 def call(String path) {
   echo "Hello, ${path}"
-  def sout = new StringBuilder(), serr = new StringBuilder()
-  def args = ['calc.exe']
-  def proc = new ProcessBuilder( args )
+  //def sout = new StringBuilder(), serr = new StringBuilder()
+  //def args = ['calc.exe']
+  //def proc = new ProcessBuilder( args )
   //Runtime.runtime.exec("cmd /c ipconfig")
-  Process process = proc.start()
-  process.consumeProcessOutput( sout, serr )
-  process.waitForOrKill( 2000 )
+  //Process process = proc.start()
+  //process.consumeProcessOutput( sout, serr )
+  //process.waitForOrKill( 2000 )
  //println System.getenv("PATH")
  // println "${path}".execute().text
+  bat 'calc.exe'
 }
 
