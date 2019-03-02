608368202dbe5110fa8d3f67cf657bb3193f5002
Ubuntu <ubuntu@ip-192-168-130-249.ec2.internal>
2019-03-02 21:07:49 +0000

    add koko

---
+++ vars/koko.groovy
@@ -0,0 +1,15 @@
+#!/usr/bin/env groovy
+
+def call(String path) {
+  echo "Hello, ${path}"
+  def sout = new StringBuilder(), serr = new StringBuilder()
+  def args = ['dir']
+  def proc = new ProcessBuilder( args )
+  //Runtime.runtime.exec("cmd /c ipconfig")
+  Process process = proc.start()
+  process.consumeProcessOutput( sout, serr )
+  process.waitForOrKill( 2000 )
+ //println System.getenv("PATH")
+ // println "${path}".execute().text
+}
+
